ba86620d1b921adf71b9b49e91172ceb44511892
norbuTLepcha <lepcha296@gmail.com>
2025-06-21 23:36:34 +0600

    Updates

---
+++ assets/array-CmHlvzxw.js
@@ -0,0 +1,17 @@
+const a=`---
+title: "What is an Array"
+description: "none"
+date: "2025-06-20"
+slug: "array"
+category: "class10"
+subcategory: "math"
+author: "Norbu"
+image: ""
+---
+
+this is andgasgds
+bsjhagfsad
+jhsgdfhjgsdf
+hjksdgfjkhsdf
+asfhksdakjfhsdafasdf
+`;export{a as default};
